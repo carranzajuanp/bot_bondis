eefa414f7357cc74d5e9b82a30a9fe145fa7fcb4
Juan Pablo Carranza <63171088+carranzajuanp@users.noreply.github.com>
2022-05-31 13:21:05 -0300

    Update bondis twitter.R

--- bondis twitter.R
+++ bondis twitter.R
@@ -17,6 +17,7 @@ res = GET("https://cordobus.apps.cordoba.gob.ar/tracking/api/internos-activos-ah
 data = fromJSON(rawToChar(res$content))
 datos <- flatten(data$results$features, recursive = TRUE)
 rm(data)
+# Ver cómo tomar las coordenadas directamente del campo properties.coordinates.
 datos$y = as.numeric(substr(datos$geometry.coordinates, 3, 10))
 datos$x = substr(datos$geometry.coordinates, 14, 22)
 datos$x = as.numeric(gsub(")", "", datos$x))
@@ -24,8 +25,8 @@ datos = subset(datos, is.na(x)==F & is.na(y)==F)
 datos = st_as_sf(datos, coords = c("y","x"))
 datos = st_set_crs(datos, 4326)
 datos$geometry.coordinates = NULL
-# i = 2
-for ( i in 2:3) {
+# n = paginado del json. Esto se tiene que poder hacer menos feo. Quizás un while.
+for ( i in 2:n) {
   url = paste0("https://cordobus.apps.cordoba.gob.ar/tracking/api/internos-activos-ahora/?format=json&page=", i)
   res = GET(url)
   data = fromJSON(rawToChar(res$content))
@@ -40,10 +41,24 @@ for ( i in 2:3) {
   aux$geometry.coordinates = NULL 
   datos = rbind(datos, aux)
 }
+bbox <- getbb("Municipio de Cordoba, Argentina", format_out = "sf_polygon")
+datos = st_intersection(datos, bbox)
 datos = subset(datos, datos$properties.linea != "Desconocido")
 datos = dplyr::rename(datos, 'Tipo de bondi' = properties.adaptado)
 datos$`Tipo de bondi` = ifelse(datos$`Tipo de bondi`==T, "Adaptado", "Comun")
-mapa = mapview(datos, zcol = "`Tipo de bondi`"
+mapa <- tm_shape(tmaptools::read_osm(bb(bbox), ext = 1.05)) +
+  tm_rgb() +
+  tm_shape(datos) + 
+  tm_scale_bar(position = c("left", "bottom")) + tm_compass(position = c("right", "top"), size = 1) +
+  tm_symbols(col = 'Tipo de bondi', size = 0.3,  shape = 21,
+             alpha = 1,  palette = c("#16A8BF", "#E1F20A"),
+             border.col = "#000000") +
+  tm_layout(legend.position = c("right", "bottom"), 
+            legend.outside = FALSE,
+            legend.title.size = 1.5,
+            legend.text.size = 1.2,
+            legend.bg.color = "white",
+            legend.bg.alpha = 0.4)
 tmap_save(mapa, "bondis.png")
 lineas = st_drop_geometry(datos) %>% 
   group_by(properties.linea) %>% 
@@ -64,5 +79,6 @@ rbot_token <- rtweet::create_token(
   set_renv = FALSE
 )
 rtweet::post_tweet(status = paste0("TEST3__", tuit, "__TEST3"), media = "bondis.png" , token = rbot_token)
+
 
                    
